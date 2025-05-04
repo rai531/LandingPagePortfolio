30933137f7f8d957f8f169235367e7c83978a684
practica1 <raimundo.monsalve531@gmail.com>
2025-05-03 20:40:42 -0400

    creamos estructura con Angular, creamos el home page y enrutamos home como vista principal

---
+++ PortafolioWeb/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';  
+import { Component } from '@angular/core';
+
+
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },
+
+
+];
