2f1d77e5c7ef73748b82c8906c931d4c35d45367
practica1 <raimundo.monsalve531@gmail.com>
2025-05-03 22:07:00 -0400

    se crea estructura html de landinpage y se genera enrutamiento de las vistas

--- PortafolioWeb/src/app/app.routes.ts
+++ PortafolioWeb/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';  
+import { HomeComponent } from './pages/home/home.component';  
 import { Component } from '@angular/core';
-
+import { EstudiosComponent } from './pages/estudios/estudios.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 export const routes: Routes = [
 
     { path: '', component: HomeComponent },
-
-
+    { path: 'estudios', component : EstudiosComponent },
+    { path: 'contacto', component : ContactoComponent },
+    { path: '**', redirectTo: '' },
+    
 ];
---
+++ PortafolioWeb/src/app/pages/estudios/estudios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudios',
+  standalone: true,
+  imports: [],
+  templateUrl: './estudios.component.html',
+  styleUrl: './estudios.component.css'
+})
+export class EstudiosComponent {
+
+}
